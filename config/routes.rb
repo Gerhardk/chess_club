25406c6d008153562e49ca225a023fd6cb287e2a
Gerhard Windows <koekies19@gmail.com>
2020-09-14 20:34:04 +0200

    Add devise to app so that club admin can login

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  devise_for :users, controllers: {
+    sessions: 'users/sessions'
+  }
   root to: 'leaderboard#show'
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,17 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_13_200829) do
+ActiveRecord::Schema.define(version: 2020_09_14_181117) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "matches", force: :cascade do |t|
+    t.bigint "player_one_id"
+    t.bigint "player_two_id"
     t.date "date"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.bigint "player_two_id"
-    t.bigint "player_one_id"
     t.integer "outcome"
     t.integer "winner_id"
     t.integer "loser_id"
@@ -37,6 +37,18 @@ ActiveRecord::Schema.define(version: 2020_09_13_200829) do
     t.integer "current_rank"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
 end
