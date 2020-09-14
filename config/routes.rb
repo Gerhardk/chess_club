e451aac24f4b03da428fe84197ee8ec1d2113018
Gerhard Windows <koekies19@gmail.com>
2020-09-14 20:47:10 +0200

    Add section to leaderboard to show logged in user

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   devise_for :users, controllers: {
     sessions: 'users/sessions'
   }
-  root to: 'leaderboard#show'
+  root to: 'leaderboard#show', as: "leaderboard"
 end
