f141e253908ce7ae39161a152271992b3bed2f4c
Gerhard Windows <koekies19@gmail.com>
2020-09-21 21:37:52 +0200

    Move matches.js out of packs

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -6,10 +6,6 @@
 require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
-require("channels")
 require("jquery")
 require("bootstrap")
-require("packs/matches")
-
-$(document).on('turbolinks:load', function() {
-})  
+require("../matches")
