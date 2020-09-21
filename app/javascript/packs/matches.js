bcefdbcffa92bd1eaac69cba360490433b296ff3
Gerhard Windows <koekies19@gmail.com>
2020-09-21 20:55:43 +0200

    Remove all other players from winner dropdown

--- app/javascript/packs/matches.js
+++ app/javascript/packs/matches.js
@@ -1,11 +1,21 @@
+let selectedPlayerOne, selectedPlayerTwo
+
 function showWinnerAndLoserFields() {
   var elements = $(".hidden-fields" )
   elements.removeClass("hidden-fields")
+  $("#match_winner_id").prop('disabled', false)
 }
 
 function hideWinnerAndLoserFields() {
   var elements = $("#outcome-members").children()
   elements.addClass("hidden-fields")
+  $("#match_winner_id").prop('disabled', false)
+}
+
+function updateWinnerSelectOption() {
+  if (selectedPlayerOne && selectedPlayerTwo) {
+    $("#match_winner_id").html("").append(selectedPlayerOne.clone()).append(selectedPlayerTwo.clone())
+  }
 }
 
 $(document).on('turbolinks:load', function() {
@@ -14,22 +24,21 @@ $(document).on('turbolinks:load', function() {
     showWinnerAndLoserFields()
   }
 
-  $("#match_player_one_id").on("change", "select", function() {
-    var selectedPlayerOne = $(this).children("option:selected").val()
-    alert(selectedPlayerOne);
+  $("#match_player_one_id").on("change", function() {
+    selectedPlayerOne = $(this).children("option:selected")
+    updateWinnerSelectOption()
   })
 
-  $("#match_player_two_id").on("change", "select", function() {
-    var selectedPlayerTwo = $(this).children("option:selected").val()
-    alert(selectedPlayerTwo);
-  })  
-
+  $("#match_player_two_id").on("change", function() {
+    selectedPlayerTwo = $(this).children("option:selected")
+    updateWinnerSelectOption()
+  })
 
   $("#outcome").on("change", "select", function() {
-    var selectedOutcome = $(this).children("option:selected").val()
-    alert(selectedOutcome)
+    let selectedOutcome = $(this).children("option:selected").val()
     if (selectedOutcome == "win") {
       showWinnerAndLoserFields()
+      updateWinnerSelectOption()
     } else {
       hideWinnerAndLoserFields()
     }
